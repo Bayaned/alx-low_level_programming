e41359fd34fb6512a8744bed60545d82d7026879
Bayane <bayanabayana745@gmail.com>
2023-03-22 17:47:32 -0700

    task1and2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+/**
+ * print_alphabet - Program for task
+ * Return: 0 (Success)
+ */
+void print_alphabet(void);
+{
+char v;
+for
+	(v = 'a';
+	 v <= 'z';
+	 v++);
+{
+_putchar(v);
+}
+_putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+/**
+ * print_alphabet_x10 - Program for task
+ * Return: 0 (Success)
+ */
+void print_alphabet_x10(void);
+{
+int i = 0;
+while (i < 5)
+{
+for
+(v = 'a';
+v <= 'z';
+v++);
+{
+_putchar(v);
+}
+_putchar(i);
+}
+return (0);
+}
