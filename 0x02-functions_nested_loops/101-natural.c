c548b42096862cabd83377ad3c4503b0b522b76c
Bayane <bayanabayana745@gmail.com>
2023-04-03 16:18:47 -0700

    add 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 /**
-* main - program computes and prints the sum of the mult of 3 or 5 < 1024
-* Return: Always 0
-*/
+ * main - main function
+ *
+ * Return: always 0
+ */
 int main(void)
 {
-int sum3 = 0;
-int sum5 = 0;
-for (int i = 3; i < 1024; i += 3)
-{
-sum3 = sum3 + i;
-}
-for (int j = 5; j < 1024; j += 5)
-{
-sum5 = sum5 + j;
-}
-int sum = sum3 + sum5;
-printf("%d\n", sum);
-return (0);
+	int a;
+	int b = 0;
+
+	for (a = 0; a < 1024; a++)
+	{
+		if (a % 3 == 0 || a % 5 == 0)
+		{
+			b += a;
+		}
+
+	}
+	printf("%d\n", b);
+	return (0);
 }
