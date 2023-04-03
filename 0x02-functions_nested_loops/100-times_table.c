56e175e880147afa1d01f36ce535d9ea4b2dbf72
Bayane <bayanabayana745@gmail.com>
2023-04-03 08:16:03 -0700

    r

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,45 +6,44 @@
  */
 void print_times_table(int n)
 {
-	int i;
-	int j;
-	if (n <= 15 && n >= 0)
-	{
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-			{
-				int w = i * j;
-				if (j == 0)
-				{
-					_putchar('0');
-				}
-				else if (w <= 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + w);
-				}
-				else if (w <= 99 && w > 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + (w / 10));
-					_putchar('0' + (w % 10));
-				}
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar('0' + (w / 100));
-					_putchar('0' + ((w / 10) % 10));
-					_putchar('0' + (w % 10));
-				}
-			}
-			_putchar('\n');
-		}
-	}
+int i, j;
+if (n <= 15 && n >= 0)
+{
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+int w = i * j;
+if (j == 0)
+{
+_putchar('0');
+}
+else if (w <= 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar('0' + w);
+}
+else if (w <= 99 && w > 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar('0' + (w / 10));
+_putchar('0' + (w % 10));
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar('0' + (w / 100));
+_putchar('0' + ((w / 10) % 10));
+_putchar('0' + (w % 10));
+}
+}
+_putchar('\n');
+}
+}
 }
