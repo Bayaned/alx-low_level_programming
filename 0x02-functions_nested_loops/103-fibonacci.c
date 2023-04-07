e663bb7cb15cbbc96395e77d9d657e449afe2b48
Bayana <bayanabayana745@gmail.com>
2023-04-06 17:36:19 -0700

    fix betty

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 /**
-* main - Sum of even-valued terms in the Fibonacci sequence that do not exceed 4,000,000
+* main - Sum of even-valued terms in the Fibonacci
 *
 * Return: Always 0
 */
 int main(void)
 {
-    long int a = 1;
-    long int b = 2;
-    long int c = 2;
-    long int sum = 2;
+	long int a = 1;
+	long int b = 2;
+	long int c = 2;
+	long int sum = 2;
 
-    while (c <= 4000000)
-    {
-        c = a + b;
-        if (c % 2 == 0)
-        {
-            sum += c;
-        }
-        a = b;
-        b = c;
-    }
+	while (c <= 4000000)
+	{
+		c = a + b;
+		if (c % 2 == 0)
+		{
+			sum += c;
+		}
+		a = b;
+		b = c;
+	}
 
-    printf("%ld\n", sum);
+	printf("%ld\n", sum);
 
-    return (0);
+	return (0);
 }
