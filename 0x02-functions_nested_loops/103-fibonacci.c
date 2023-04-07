c905bc4aa97ecae927146db44b1eefbbbccf3955
Bayana <bayanabayana745@gmail.com>
2023-04-06 17:22:06 -0700

    hope so

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,16 +9,16 @@ int main(void)
 long int a = 1;
 long int b = 2;
 long int c;
-long int sum = 0;
+long int sum = 2;
 while (c < 4000000)
 {
+if (c % 2 == 0)
+{
+sum += c;
+}
 c = a + b;
 a = b;
 b = c;
-if ((c % 2) == 0)
-{
-sum = sum + c;
-}
 }
 printf("%ld\n", sum);
 return (0);
