57b43141190778bdae70cafdf8e240f624b49a89
Bayana <bayanabayana745@gmail.com>
2023-04-06 17:29:45 -0700

    finaly

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 /**
-* main - even-valued terms Fibonacci numbers
+* main - Sum of even-valued terms in the Fibonacci sequence that do not exceed 4,000,000
 *
-* Return: nothing
+* Return: Always 0
 */
 int main(void)
 {
-long int a = 1;
-long int b = 2;
-long int c;
-long int sum = 2;
-while (c < 4000000)
-{
-if (c % 2 == 0)
-{
-sum += c;
-}
-c = a + b;
-a = b;
-b = c;
-}
-printf("%ld\n", sum);
-return (0);
+    long int a = 1;
+    long int b = 2;
+    long int c = 2;
+    long int sum = 2;
+
+    while (c <= 4000000)
+    {
+        c = a + b;
+        if (c % 2 == 0)
+        {
+            sum += c;
+        }
+        a = b;
+        b = c;
+    }
+
+    printf("%ld\n", sum);
+
+    return (0);
 }
