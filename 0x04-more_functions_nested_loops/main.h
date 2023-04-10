c15578b75a92e479469e5fa59ceb42c315d02f35
Bayana <bayanabayana745@gmail.com>
2023-04-10 05:41:33 -0700

    Until 2

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,60 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+/**
+ * _putchar - writes the character c to std
+ * @c: parameter to compare
+ * Return: 1 or 0
+ */
+int _putchar(char c);
+/**
+ * _isupper - checks for uppercase
+ * @c: character to check
+ * Return: true or false
+ */
+int _isupper(int c);
+/**
+ * _isdigit -  checks for a digit
+ * @c: character to check
+ * Return: true or false
+ */
+int _isdigit(int c);
+/**
+ * mul - multiplies two integers
+ * @a: integer to multiply
+ * @b: integer to multiply
+ * Return: result
+ */
+int mul(int a, int b);
+/**
+ * print_numbers - print numbers
+ */
+void print_numbers(void);
+/**
+ * print_most_numbers - print numbers
+ */
+void print_most_numbers(void);
+/**
+ * more_numbers - print numbers
+ */
+void more_numbers(void);
+/**
+ * print_line - print line
+ * @n: integer to print line
+ */
+void print_line(int n);
+/**
+ * print_diagonal - print diagonal
+ * @n: integer to print lines
+ */
+void print_diagonal(int n);
+/**
+ * print_square - print squar
+ * @size: print the size of square
+ */
+void print_square(int size);
+/**
+ * print_triangle - print trianhgle
+ * @size: print the size of triangle
+ */
+void print_triangle(int size);
+#endif
