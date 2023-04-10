6bb3cec81eb4f60f280c22270678e9fdb67b8b73
Bayana <bayanabayana745@gmail.com>
2023-04-10 08:44:47 -0700

    from 8 to 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_triangle - print trianhgle
+ * @size: print the size of triangle
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				if (i + j < size - 1)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+		_putchar('\n');
+		}
+	}
+}
