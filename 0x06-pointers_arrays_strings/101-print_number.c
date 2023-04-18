af604d934a35c249d26c4f8df2abb18251fddceb
Bayana <bayanabayana745@gmail.com>
2023-04-18 15:31:22 -0700

    poue

--- 0x06-pointers_arrays_strings/101-print_number.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-/**
- * print_number - function that prints an integer
- * @n: integer to print
- */
-void print_number(int n)
-{
-}
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,15 +8,15 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int j;
+	int i;
 
-	for (j = 0; src[j] != '\0'; j++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		if (j < n)
-		{
-			dest[j] = src[j];
-		}
+		dest[i] = src[i];
 	}
-	dest[j] = '\0';
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
 	return (dest);
 }
