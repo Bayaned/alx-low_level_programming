8ded02b3ca0ddef6f71ba7dccc55bef1724eda25
Bayane <bayanabayana745@gmail.com>
2023-06-07 16:51:35 -0700

    from 0 to 4

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - function that returns the natural square root of a number
+ * @n: number
+ * Return: the square root or -1
+ */
+int _sqrt_recursion(int n)
+{
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,44 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * _puts_recursion - function that prints a string
+ * @s: string to print
+ */
+void _puts_recursion(char *s);
+/**
+ * _print_rev_recursion - function that prints a string in reverse
+ * @s: string to print in reverse
+ */
+void _print_rev_recursion(char *s);
+/**
+ * _strlen_recursion - function that returns the length of a string
+ * @s: 
+ * Return:
+ */
+int _strlen_recursion(char *s);
+/**
+ *
+ * @
+ * Return:
+ */
+int factorial(int n);
+/**
+ *
+ * @
+ * Return:
+ */
+int _pow_recursion(int x, int y);
+/**
+ *
+ * @
+ * Return:
+ */
+int _sqrt_recursion(int n);
+/**
+ *
+ * @
+ * Return:
+ */
+int is_prime_number(int n);
+int _putchar(char c);
+#endif
