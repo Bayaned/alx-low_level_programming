3c1d683b3603d86b42fb740a7c32c8f57a710d68
Bayane <bayanabayana745@gmail.com>
2023-06-08 14:55:19 -0700

    6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,3 +6,35 @@
  */
 int is_prime_number(int n)
 {
+	int i = 2;
+
+	if (n < 2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (tryit(i, n));
+	}
+}
+/**
+ * tryit - to try
+ * @i: to try
+ * @n: integer I'm testing
+ * Return: value
+ */
+int tryit(int i, int n)
+{
+	if (n % i == 0 && n != i)
+	{
+		return (0);
+	}
+	else if (n % i != 0 && i < n)
+	{
+		return (tryit(i + 1, n));
+	}
+	else
+	{
+		return (1);
+	}
+}
