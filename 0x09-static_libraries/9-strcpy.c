d551b217897874b9b86c854b413f32050a365ef8
Bayane <bayanabayana745@gmail.com>
2023-06-11 10:11:09 -0700

    0

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * _strcpy - function to copy
+ * @dest: destination to copy
+ * @src: pointer
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i <= _strlen(src); i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
