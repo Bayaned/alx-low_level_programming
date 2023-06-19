6f2a430c810daf3006c67b97988271d7f542fd4e
Bayane <bayanabayana745@gmail.com>
2023-06-19 08:23:13 -0700

    mandatory tasks 0 and 1

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * str_concat - function that concatenates two strings
+ * @s1: first string
+ * @s2: string 2
+ * Return: NULL or concated string
+ */
+char *str_concat(char *s1, char *s2)
